f5ab8c5a80a9f8cb8cbfc89abd17ce1c29e7bb79
Metehan Karadeniz <metehan8400@gmail.com>
2023-01-17 13:55:04 +0300

    upload

---
+++ presentFromLeftToRight/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  presentFromLeftToRight
+//
+//  Created by Metehan Karadeniz on 10.01.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    @IBAction func dismissBttn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+        
+        
+        
+    }
+    
+  
+
+}
--- presentFromLeftToRight/ViewController.swift
+++ presentFromLeftToRight/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func bttnClickedForPresent(_ sender: Any) {
+        let secondVC = self.storyboard?.instantiateViewController(withIdentifier: "secondViewController") as! SecondViewController
+        secondVC.modalPresentationStyle = .custom
+        secondVC.transitioningDelegate = self
 
+        self.present(secondVC, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: UIViewControllerTransitioningDelegate {
+
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return PresentTransition()
+    }
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return DismissTransition()
+    }
+}
